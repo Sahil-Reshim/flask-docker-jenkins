7ab1d8c09fb726afb121741a6a11e6a424117352
Sahil-Reshim <sahilreshim441@gmail.com>
2026-04-27 14:25:56 +0530

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask App deployed using Jenkins, Docker and GitHub! - Made Some Chnages"
+    return "Flask App deployed using Jenkins, Docker, GitHub, Gunicorn, Nginx and DockerHub!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
